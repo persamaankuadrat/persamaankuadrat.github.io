87ea467076f64ee6564e7290e3a5555a02d0a19d
persamaankuadrat <lili.rika.yulia@gmail.com>
2021-08-09 14:40:29 +0800

    hhhh

--- isimateri.js
+++ isimateri.js
@@ -19,10 +19,24 @@ function ftampil(){
 
 var x = document.getElementById("isimateri");
 var xx= document.querySelector('.isimateri');
-
+var x1 = document.getElementById("petunjuk");
+var xx1= document.querySelector('.petunjuk');
+var x2 = document.getElementById("petunjuk2");
+var xx2= document.querySelector('.petunjuk2');
+var x3 = document.getElementById("petunjuk3");
+var xx3= document.querySelector('.petunjuk3');
 xx.addEventListener("click",function(){
   x.classList.toggle("hilang");
 })
+xx1.addEventListener("click",function(){
+	x1.classList.toggle("hilang");
+  })
+  xx2.addEventListener("click",function(){
+	x2.classList.toggle("hilang");
+  })
+  xx3.addEventListener("click",function(){
+	x3.classList.toggle("hilang");
+  })
 
 let jjj = document.querySelector(".jjj");
 let btn =document.querySelector(".btn");
---
+++ jsno3faktor.js
@@ -0,0 +1,34 @@
+let input1 = document.getElementById("texta");
+let input1a = document.getElementById("textb");
+let btn1 = document.getElementById("ceka");
+let btn1a = document.getElementById("cekb");
+let aksi1 = document.getElementById("ketx1");
+let aksi1a = document.getElementById("ketx2");
+
+btn1.addEventListener("click",function(){
+    if(input1.value == "3" ){
+        aksi1.innerHTML = `<img src="benar.png" >`
+    }
+    else{
+        aksi1.innerHTML = `<img src="salah.png" >`
+    }
+    MathJax.typeset();
+  })
+  btn1a.addEventListener("click",function(){
+    if(input1a.value == "3" ){
+        aksi1a.innerHTML = `<img src="benar.png" >`
+    }
+    else{
+        aksi1a.innerHTML = `<img src="salah.png" >`
+    }
+    MathJax.typeset();
+  })
+  input1.addEventListener("keyup",function(){
+    input1.value = input1.value.split(' ').join('');
+  })
+  input1a.addEventListener("keyup",function(){
+    input1a.value = input1a.value.split(' ').join('');
+  })
+
+
+
