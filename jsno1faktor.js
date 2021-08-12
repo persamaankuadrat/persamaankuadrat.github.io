d073fb358a4721127e1625dc71f13db576a91d55
persamaankuadrat <lili.rika.yulia@gmail.com>
2021-08-12 14:53:10 +0800

    hhhh

--- jsno1faktor.js
+++ jsno1faktor.js
@@ -1,31 +1,31 @@
 
-  // var slideIndex = 1;
-  // showSlides(slideIndex);
+  var slideIndex = 1;
+  showSlides(slideIndex);
   
-  // function plusSlides(n) {
-  //   showSlides(slideIndex += n);
-  // }
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
   
-  // function currentSlide(n) {
-  //   showSlides(slideIndex = n);
-  // }
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
   
   
-  // function showSlides(n) {
-  //   var i;
-  //   var slides = document.getElementsByClassName("mySlides");
-  //   var dots = document.getElementsByClassName("dot");
-  //   if (n > slides.length) {slideIndex = 1}    
-  //   if (n < 1) {slideIndex = slides.length}
-  //   for (i = 0; i < slides.length; i++) {
-  //       slides[i].style.display = "none";  
-  //   }
-  //   for (i = 0; i < dots.length; i++) {
-  //       dots[i].className = dots[i].className.replace(" active2", "");
-  //   }
-  //   slides[slideIndex-1].style.display = "block";  
-  //   dots[slideIndex-1].className += " active2";
-  // }
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active2", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active2";
+  }
 
 let input1 = document.getElementById("texta");
 let input1a = document.getElementById("textb");
